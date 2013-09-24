2c28a6306775f7cfbf4410fe9e2e313dc6abd267
stakemori <stakemorii@gmail.com>
2013-09-24 15:41:03 +0900

    Added CuspFormsDegree2 to all.py.

--- all.py
+++ all.py
@@ -3,4 +3,4 @@ from deg2_fourier import diff_opetator_4
 from deg2_fourier import deg2_fc_set_number_of_threads
 from deg2_fourier import KlingenEisensteinAndCuspForms
 from deg2_fourier import eisenstein_series_degree2
-from deg2_fourier import x10_with_prec, x12_with_prec, x35_with_prec, load_deg2_modular_form, Y12_with_prec, rankin_cohen_pair_sym, rankin_cohen_pair_det2_sym, rankin_cohen_triple_det_sym4
+from deg2_fourier import x10_with_prec, x12_with_prec, x35_with_prec, load_deg2_modular_form, Y12_with_prec, rankin_cohen_pair_sym, rankin_cohen_pair_det2_sym, rankin_cohen_triple_det_sym4, CuspFormsDegree2
