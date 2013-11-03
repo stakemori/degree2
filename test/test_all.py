bced55e9967b0ab9a33ce3cf1881898f37a39f01
stakemori <stakemorii@gmail.com>
2013-11-03 13:40:41 +0900

    Added test_all.

---
+++ test/test_all.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8; mode: sage -*-
+import degree2.test.test_eigenforms
+import degree2.test.misc_test
+import degree2.test.test_fc_mul_add
+import degree2.test.test_gens
+import degree2.test.test_prec_class
