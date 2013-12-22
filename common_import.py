d7237c6df988403e948fa33ff6ca4bb2811de23b
stakemori <stakemorii@gmail.com>
2013-12-22 21:58:07 +0900

    Stop importing tail_recursive.

--- common_import.py
+++ common_import.py
@@ -4,4 +4,3 @@ import sage
 from sage.all import floor, sqrt, QQ, RR, CC, ZZ, preparse, quadratic_L_function__exact, kronecker_character, fundamental_discriminant, prime_factors, valuation, PolynomialRing, CuspForms, matrix, vector, MatrixSpace, prime_range, parallel, gcd, zeta, divisors, factor, diagonal_matrix, is_prime, infinity, mod, identity_matrix, save, load, denominator, factorial, CartesianProduct, Integer
 import operator
 from itertools import imap, groupby
-from tail_recursive import *
