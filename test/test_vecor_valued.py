706bfe8f35380c4f788d9b45de24ff66538d71e3
Sho Takemori <stakemorii@gmail.com>
2014-02-05 10:51:40 +0900

    Added a test for T(2) action on a vector valued modular form.

--- test/test_vecor_valued.py
+++ test/test_vecor_valued.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8; mode: sage -*-
 import unittest
+# from unittest import skip
+
 from degree2.deg2_fourier import (eisenstein_series_degree2,
                                   rankin_cohen_pair_sym)
-
-# from unittest import skip
+from degree2.basic_operation import PrecisionDeg2
 
 class TestVectorValued(unittest.TestCase):
     def test_vector_vald_klingen_hecke_pol(self):
@@ -14,6 +15,16 @@ class TestVectorValued(unittest.TestCase):
         x = pl.parent().gens()[0]
         f = 1 + 24*x + 2048*x**2
         self.assertTrue(f * f.subs({x: 2**8 * x}) == pl)
+
+    def test_t_2_action(self):
+        es4 = eisenstein_series_degree2(4, 10)
+        es6 = eisenstein_series_degree2(6, 10)
+        F10 = rankin_cohen_pair_sym(2, es4, es6)
+        ev2 =  -24 * (1+2**8)
+        prec5 = PrecisionDeg2(5)
+        self.assertTrue(
+            all([ F10.hecke_operator(2, t) == ev2 * F10[t] for t in prec5]))
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestVectorValued)
 unittest.TextTestRunner(verbosity = 2).run(suite)
