a0eb4241549ce29687e9e6bd59b06c37594a6d3d
Sho Takemori <stakemorii@gmail.com>
2014-02-22 10:41:29 +0900

    Modified _rankin_cohen_bracket_func.

--- deg2_fourier.py
+++ deg2_fourier.py
@@ -1589,13 +1589,13 @@ def _rankin_cohen_bracket_func(Q, rnames, unames):
     rnames = "r00, r01, r02, ..., r(n-1)0, r(n-1)1, r(n-1)2"
     unames = "u1, u2"
     Let
-    R0 = [[r00, r01/2],
-          [r01/2, r02]],
-    R1 = [[r10, r11/2],
-          [r11/2, r12]],
+    R0 = [[r00, r0],
+          [r0, r02]],
+    R1 = [[r10, r11],
+          [r11, r12]],
     ...
-    R(n-1) = [[r(n-1)0, r(n-1)1/2],
-              [r(n-1)1/2, r(n-1)2]]
+    R(n-1) = [[r(n-1)0, r(n-1)],
+              [r(n-1), r(n-1)2]]
     be the symmetric matrices.
     Q is a homogenous polynomial of u1 and u2
     whose coefficient is a polynomial of R0, ..., R(n-1).
@@ -1614,8 +1614,9 @@ def _rankin_cohen_bracket_func(Q, rnames, unames):
             psum = 0
             for longtpl, v in pol.dict().iteritems():
                 tpls = group(longtpl, 3)
-                psum += v*mul([f._differential_operator_monomial(*t)
-                               for f, t in zip(flist, tpls)])
+                psum += v * mul([QQ(2)**(-t[1]) *
+                                 f._differential_operator_monomial(*t)
+                                 for f, t in zip(flist, tpls)])
             res.append((i, psum))
         return [x[1] for x in sorted(res, key=lambda x: -x[0])]
     return rankin_cohen
@@ -1634,8 +1635,8 @@ def rankin_cohen_pair_sym(j, f, g):
     RS_ring = PolynomialRing(QQ, names=rnames)
     (r11, r12, r22, s11, s12, s22) = RS_ring.gens()
     (u1, u2) = PolynomialRing(RS_ring, names=unames).gens()
-    r = r11 * u1**2 + r12 * u1 * u2 + r22 * u2**2
-    s = s11 * u1**2 + s12 * u1 * u2 + s22 * u2**2
+    r = r11 * u1**2 + 2 * r12 * u1 * u2 + r22 * u2**2
+    s = s11 * u1**2 + 2 * s12 * u1 * u2 + s22 * u2**2
     k = f.wt
     l = g.wt
     m = j//2
@@ -1657,8 +1658,8 @@ def rankin_cohen_pair_det2_sym(j, f, g):
     RS_ring = PolynomialRing(QQ, names=rnames)
     (r11, r12, r22, s11, s12, s22) = RS_ring.gens()
     (u1, u2) = PolynomialRing(RS_ring, names=unames).gens()
-    r = r11 * u1**2 + r12 * u1 * u2 + r22 * u2**2
-    s = s11 * u1**2 + s12 * u1 * u2 + s22 * u2**2
+    r = r11 * u1**2 + 2 * r12 * u1 * u2 + r22 * u2**2
+    s = s11 * u1**2 + 2 * s12 * u1 * u2 + s22 * u2**2
     k = f.wt
     l = g.wt
     m = j//2
@@ -1668,17 +1669,16 @@ def rankin_cohen_pair_det2_sym(j, f, g):
               i * r**(i - 1) * s**(m - i) for i in range(1, m + 1)])
     Qy = sum([(-1)**i * combination(m + l, i) * combination(m + k, m - i) *
               (m - i) * r**i * s**(m - i - 1) for i in range(0, m)])
-    detR = r11 * r22 - QQ(4)**(-1) * r12**2
-    detS = s11 * s22 - QQ(4)**(-1) * s12**2
+    detR = r11 * r22 - r12**2
+    detS = s11 * s22 - s12**2
     # det(R+S)
-    detRpS = -QQ(1)/QQ(4)*r12**2 + r11*r22 + r22*s11 - \
-        QQ(1)/QQ(2)*r12*s12 - QQ(1)/QQ(4)*s12**2 + r11*s22 + s11*s22
-
-    Q2 = (2*k - 1) * (2*l - 1) * detRpS - (2*k - 1) * (2*k + 2*l - 1)\
-        * detS - (2*l - 1)*(2*k + 2*l - 1)*detR
-    Q = QQ(4)**(-1) * Q2 * Q +\
-        QQ(2)**(-1) * ((2*l - 1) * detR * s - (2*k - 1) * detS * r) * \
-        (Qx - Qy)
+    detRpS = (-r12**2 + r11*r22 + r22*s11 - QQ(2) *r12*s12
+               - s12**2 + r11*s22 + s11*s22)
+    Q2 = ((2*k - 1) * (2*l - 1) * detRpS - (2*k - 1) * (2*k + 2*l - 1) * detS -
+          (2*l - 1)*(2*k + 2*l - 1)*detR)
+    Q = (QQ(4)**(-1) * Q2 * Q +
+         QQ(2)**(-1) * ((2*l - 1) * detR * s -
+                        (2*k - 1) * detS * r) * (Qx - Qy))
     args = [f, g]
     forms = _rankin_cohen_bracket_func(Q, rnames, unames)(args)
     prec = common_prec(args)
@@ -1696,73 +1696,75 @@ def rankin_cohen_triple_det_sym4(f, g, h):
 
     R = PolynomialRing(QQ, names=rnames)
     S = PolynomialRing(R, names=unames)
-    # det = naive_det_func(3)
 
     (r11, r12, r22, s11, s12, s22, t11, t12, t22) = R.gens()
     (u1, u2) = S.gens()
 
-    # m00 = [[(k1 + 1)*r11, k2,           k3],
-    #        [r11**2,       s11,          t11],
-    #        [r11*r12,      s12,          t12]]
-    # m01 = [[k1,           (k2 + 1)*s11, k3],
-    #        [r11,          s11**2,       t11],
-    #        [r12,          s11*s12,      t12]]
+    m00 = matrix([[(k1 + 1)*r11, k2, k3],
+                  [r11**2, s11, t11],
+                  [r11*r12, s12, t12]])
 
-    # m10 = [[(k1 + 1)*r12, k2,           k3],
-    #        [r11*r12,      s11,          t11],
-    #        [r12**2,       s12,          t12]]
-    # m11 = [[k1,           (k2 + 1)*s12, k3],
-    #        [r11,          s11*s12,      t11],
-    #        [r12,          s12**2,       t12]]
-    # m12 = [[(k1 + 1)*r11, k2 ,          k3],
-    #        [r11**2,       s11,          t11],
-    #        [r11*r22,      s22,          t22]]
-    # m13 = [[k1,           (k2 + 1)*s11, k3],
-    #        [r11,          s11**2,       t11],
-    #        [r22,          s11*s22,      t22]]
+    m01 = matrix([[k1, (k2 + 1)*s11, k3],
+                 [r11, s11**2, t11],
+                 [r12, s11*s12, t12]])
 
-    # m20 = [[(k1 + 1)*r12, k2,           k3],
-    #        [r11*r12,      s11,          t11],
-    #        [r22*r12,      s22,          t22]]
-    # m21 = [[k1,           (k2 + 1)*s12, k3],
-    #        [r11,          s11*s12,      t11],
-    #        [r22,          s22*s12,      t22]]
+    m10 = matrix([[(k1 + 1)*r12, k2, k3],
+                 [r11*r12, s11, t11],
+                 [r12**2, s12, t12]])
 
-    # m30 = [[(k1 + 1)*r12, k2,           k3],
-    #        [r12**2,       s12,          t12],
-    #        [r12*r22,      s22,          t22]]
-    # m31 = [[k1,           (k2 + 1)*s12, k3],
-    #        [r12,          s12**2,       t12],
-    #        [r22,          s12*s22,      t22]]
-    # m32 = [[(k1 + 1)*r22, k2,           k3],
-    #        [r11*r22,      s11,          t11],
-    #        [r22**2,       s22,          t22]]
-    # m33 = [[k1,           (k2 + 1)*s22, k3],
-    #        [r11,          s11*s22,      t11],
-    #        [r22,          s22**2,       t22]]
+    m11 = matrix([[k1, (k2 + 1)*s12, k3],
+                 [r11, s11*s12, t11],
+                 [r12, s12**2, t12]])
 
-    # m40 = [[(k1 + 1)*r22, k2,           k3],
-    #        [r22*r12,      s12,          t12],
-    #        [r22**2,       s22,          t22]]
-    # m41 = [[k1,           (k2 + 1)*s22, k3],
-    #        [r12,          s22*s12,      t12],
-    #        [r22,          s22**2,       t22]]
+    m12 = matrix([[(k1 + 1)*r11, k2, k3],
+                 [r11**2, s11, t11],
+                 [r11*r22, s22, t22]])
 
-    # Q0 = (k2 + 1)*det(m00) - (k1 + 1)*det(m01)
-    # Q1 = 2*(k2 + 1)*det(m10) - 2*(k1 + 1)*det(m11) \
-    #     + (k2 + 1)*det(m12) - (k1 + 1)*det(m13)
-    # Q2 = 3*(k2 + 1)*det(m20) - 3*(k1 + 1)*det(m21)
-    # Q3 = 2*(k2 + 1)*det(m30) - 2*(k1 + 1)*det(m31) \
-    #     + (k2 + 1)*det(m32) - (k1 + 1)*det(m33)
-    # Q4 = (k2 + 1)*det(m40) - (k1 + 1)*det(m41)
+    m13 = matrix([[k1, (k2 + 1)*s11, k3],
+                 [r11, s11**2, t11],
+                 [r22, s11*s22, t22]])
 
-    # Q = Q0*u1**4 + Q1*u1**3*u2 + Q2*u1**2*u2**2 + Q3*u1*u2**3 + Q4*u2**4
-    # Definition of Q is above.
-    Q = ((k2 + 1)*((r12*r22*s22 - r22**2*s12)*k3 - ((k1 + 1)*r22*s22 - k2*r22**2)*t12 + ((k1 + 1)*r22*s12 - k2*r12*r22)*t22) - (k1 + 1)*((r12*s22**2 - r22*s12*s22)*k3 + ((k2 + 1)*r22*s22 - k1*s22**2)*t12 - ((k2 + 1)*r12*s22 - k1*s12*s22)*t22))*u2**4 + 3*((k2 + 1)*((r11*r12*s22 - r12*r22*s11)*k3 - ((k1 + 1)*r12*s22 - k2*r12*r22)*t11 + ((k1 + 1)*r12*s11 - k2*r11*r12)*t22) - (k1 + 1)*((r11*s12*s22 - r22*s11*s12)*k3 + ((k2 + 1)*r22*s12 - k1*s12*s22)*t11 - ((k2 + 1)*r11*s12 - k1*s11*s12)*t22))*u1**2*u2**2 + ((k2 + 1)*((r11**2*s12 - r11*r12*s11)*k3 - ((k1 + 1)*r11*s12 - k2*r11*r12)*t11 + ((k1 + 1)*r11*s11 - k2*r11**2)*t12) - (k1 + 1)*((r11*s11*s12 - r12*s11**2)*k3 + ((k2 + 1)*r12*s11 - k1*s11*s12)*t11 - ((k2 + 1)*r11*s11 - k1*s11**2)*t12))*u1**4 + ((k2 + 1)*((r11*r22*s22 - r22**2*s11)*k3 - ((k1 + 1)*r22*s22 - k2*r22**2)*t11 + ((k1 + 1)*r22*s11 - k2*r11*r22)*t22) + 2*(k2 + 1)*((r12**2*s22 - r12*r22*s12)*k3 - ((k1 + 1)*r12*s22 - k2*r12*r22)*t12 + ((k1 + 1)*r12*s12 - k2*r12**2)*t22) - (k1 + 1)*((r11*s22**2 - r22*s11*s22)*k3 + ((k2 + 1)*r22*s22 - k1*s22**2)*t11 - ((k2 + 1)*r11*s22 - k1*s11*s22)*t22) - 2*(k1 + 1)*((r12*s12*s22 - r22*s12**2)*k3 + ((k2 + 1)*r22*s12 - k1*s12*s22)*t12 - ((k2 + 1)*r12*s12 - k1*s12**2)*t22))*u1*u2**3 + ((k2 + 1)*((r11**2*s22 - r11*r22*s11)*k3 - ((k1 + 1)*r11*s22 - k2*r11*r22)*t11 + ((k1 + 1)*r11*s11 - k2*r11**2)*t22) + 2*(k2 + 1)*((r11*r12*s12 - r12**2*s11)*k3 - ((k1 + 1)*r12*s12 - k2*r12**2)*t11 + ((k1 + 1)*r12*s11 - k2*r11*r12)*t12) - (k1 + 1)*((r11*s11*s22 - r22*s11**2)*k3 + ((k2 + 1)*r22*s11 - k1*s11*s22)*t11 - ((k2 + 1)*r11*s11 - k1*s11**2)*t22) - 2*(k1 + 1)*((r11*s12**2 - r12*s11*s12)*k3 + ((k2 + 1)*r12*s12 - k1*s12**2)*t11 - ((k2 + 1)*r11*s12 - k1*s11*s12)*t12))*u1**3*u2 # flake8: noqa
-    Q = sum([v.subs({r12: QQ(2)**(-1) * r12,
-                     s12: QQ(2)**(-1) * s12,
-                     t12: QQ(2)**(-1) * t12}) * u1**i * u2**j
-             for (i, j), v in Q.dict().iteritems()])
+    m20 = matrix([[(k1 + 1)*r12, k2, k3],
+                 [r11*r12, s11, t11],
+                 [r22*r12, s22, t22]])
+
+    m21 = matrix([[k1, (k2 + 1)*s12, k3],
+                 [r11, s11*s12, t11],
+                 [r22, s22*s12, t22]])
+
+    m30 = matrix([[(k1 + 1)*r12, k2, k3],
+                 [r12**2, s12, t12],
+                 [r12*r22, s22, t22]])
+
+    m31 = matrix([[k1, (k2 + 1)*s12, k3],
+                 [r12, s12**2, t12],
+                 [r22, s12*s22, t22]])
+
+    m32 = matrix([[(k1 + 1)*r22, k2, k3],
+                 [r11*r22, s11, t11],
+                 [r22**2, s22, t22]])
+
+    m33 = matrix([[k1, (k2 + 1)*s22, k3],
+                 [r11, s11*s22, t11],
+                 [r22, s22**2, t22]])
+
+    m40 = matrix([[(k1 + 1)*r22, k2, k3],
+                 [r22*r12, s12, t12],
+                 [r22**2, s22, t22]])
+
+    m41 = matrix([[k1, (k2 + 1)*s22, k3],
+                 [r12, s22*s12, t12],
+                 [r22, s22**2, t22]])
+
+    Q0 = (k2 + 1)*m00.det() - (k1 + 1)*m01.det()
+    Q1 = (2*(k2 + 1)*m10.det() - 2*(k1 + 1)*m11.det()
+          + (k2 + 1)*m12.det() - (k1 + 1)*m13.det())
+    Q2 = 3*(k2 + 1)*m20.det() - 3*(k1 + 1)*m21.det()
+    Q3 = (2*(k2 + 1)*m30.det() - 2*(k1 + 1)*m31.det()
+          + (k2 + 1)*m32.det() - (k1 + 1)*m33.det())
+    Q4 = (k2 + 1)*m40.det() - (k1 + 1)*m41.det()
+
+    Q = Q0*u1**4 + Q1*u1**3*u2 + Q2*u1**2*u2**2 + Q3*u1*u2**3 + Q4*u2**4
     args = [f, g, h]
     forms = _rankin_cohen_bracket_func(Q, rnames, unames)(args)
     prec = common_prec(args)
