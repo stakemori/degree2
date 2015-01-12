57ae7e9114f3fd144e1f97fdae612e7b76e48e6f
Sho Takemori <stakemorii@gmail.com>
2015-01-12 13:00:07 +0900

    List module names explicitly.

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,6 +1,6 @@
 import os
 import re
-import imp
+# import imp
 
 from os.path import dirname, isfile, join
 pat = re.compile(".*test.+py$")
@@ -10,11 +10,15 @@ def test_module_names():
     return [f for f in os.listdir(this_dir) if re.match(pat, f)
             and isfile(join(this_dir, f)) and join(this_dir, f) != __file__]
 
+from degree2.tests import (test_fc_mul_add, test_eigenforms, test_divide,
+                           test_save_load, test_interpolate,
+                           test_gens, misc_test, test_prec_class,
+                           test_vecor_valued)
 
-def import_tests():
-    for f in test_module_names():
-        mod_name = "degree2.tests." + f.split(".")[0]
-        pth = join(this_dir, f)
-        imp.load_source(mod_name, pth)
+# def import_tests():
+#     for f in test_module_names():
+#         mod_name = "degree2.tests." + f.split(".")[0]
+#         pth = join(this_dir, f)
+#         imp.load_source(mod_name, pth)
 
-import_tests()
+# import_tests()
