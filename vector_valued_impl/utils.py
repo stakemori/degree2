d525079be61d29ed0dd8e25a10360278def31940
Sho Takemori <stakemorii@gmail.com>
2015-07-12 22:23:59 +0900

    Change the default path of data_dir.

--- vector_valued_impl/utils.py
+++ vector_valued_impl/utils.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8; mode: sage -*-
 import os
 
-data_dir = os.path.join(os.getenv("HOME"), "data/vector_valued_sym10")
+data_dir = os.path.join(os.getenv("HOME"), "data/vector_valued")
