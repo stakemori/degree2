e13c22aeedc786380cdc8e34e5bd043e7a1a11f6
Sho Takemori <stakemorii@gmail.com>
2015-09-21 14:37:03 +0900

    Removed unused code.

--- vector_valued_smfs.py
+++ vector_valued_smfs.py
@@ -9,29 +9,20 @@ of small degrees.
 
 import operator
 
-from sage.all import PowerSeriesRing, QQ
 from sage.misc.cachefunc import cached_method
 
 from degree2.hecke_module import HeckeModule
 from degree2.basic_operation import PrecisionDeg2
 from degree2.utils import (linearly_indep_rows_index_list,
-                           mul, is_number)
+                           is_number)
 from degree2.scalar_valued_smfs import tuples_even_wt_modular_forms
 
-from degree2.all import (rankin_cohen_pair_sym,
-                         rankin_cohen_triple_det_sym2,
-                         rankin_cohen_pair_det2_sym,
-                         rankin_cohen_triple_det_sym4)
-
 from degree2.tsushima_dimension_formula import hilbert_series_maybe
-from degree2.scalar_valued_smfs import degree2_modular_forms_ring_level1_gens \
-    as deg2_ring_gens
 from degree2.const import ConstMul, CalculatorVectValued
 from degree2.const import ScalarModFormConst as SMFC
 
 from degree2.vector_valued_impl.utils import data_dir
 
-import degree2.vector_valued_impl as vec_impl
 import degree2.vector_valued_impl.sym2.even_structure as impl_sym2_even
 import degree2.vector_valued_impl.sym2.odd_structure as impl_sym2_odd
 import degree2.vector_valued_impl.sym4.even_structure as impl_sym4_even
@@ -157,7 +148,7 @@ class GivenWtBase(VectorValuedSiegelModularForms):
     def _basis_const(self):
         '''This method should yield a generator that consists of an instance of
         ConstMul.'''
-        raise NotImplementedError
+        pass
 
     def _basis_const_base(self, ignored_dct):
         '''This method is used for implmentation of _basis_const.
@@ -188,38 +179,6 @@ class GivenWtBase(VectorValuedSiegelModularForms):
         return res
 
 
-class Sym10Even(GivenWtBase):
-
-    '''A class for the module M_{det^wt Sym(10)} where wt is even.
-    '''
-
-    def __init__(self, wt, prec, data_directory=data_dir):
-        consts = vec_impl.sym10.even_structure.gen_consts()
-        calculator = CalculatorVectValued(consts, data_directory)
-        super(Sym10Even, self).__init__(wt, prec, calculator=calculator,
-                                        gen_consts=consts)
-
-    def _basis_const(self):
-        ignored_dct = vec_impl.sym10.even_structure.ignored_dct()
-        return self._basis_const_base(ignored_dct)
-
-
-class Sym10Odd(GivenWtBase):
-
-    '''A class for the module M_{det^wt Sym(10)} where wt is odd.
-    '''
-
-    def __init__(self, wt, prec, data_directory=data_dir):
-        consts = vec_impl.sym10.odd_structure.gen_consts()
-        calculator = CalculatorVectValued(consts, data_directory)
-        super(Sym10Odd, self).__init__(wt, prec, calculator=calculator,
-                                       gen_consts=consts)
-
-    def _basis_const(self):
-        ignored_dct = vec_impl.sym10.odd_structure.ignored_dct()
-        return self._basis_const_base(ignored_dct)
-
-
 def sym_j_give_wt_class(j, parity, gen_consts, ignored_dct,
                         data_directory=data_dir):
     '''Return a class for the module M_{det^wt Sym(j)}.
@@ -242,120 +201,3 @@ def sym_j_give_wt_class(j, parity, gen_consts, ignored_dct,
 def calculator_symj(j, parity, data_directory=data_dir):
     consts, _ = _consts_i_dct()[j, parity]
     return CalculatorVectValued(consts, data_directory)
-
-
-class VvsmfSym2_4(VectorValuedSiegelModularForms):
-
-    def basis(self):
-        raise NotImplementedError
-
-    def dimension(self):
-        '''
-        Returns the dimension of self.
-        '''
-        R = PowerSeriesRing(QQ, names="t", default_prec=self.wt + 1)
-        t = R.gens()[0]
-        dnm = (1 - t ** 4) * (1 - t ** 6) * (1 - t ** 10) * (1 - t ** 12)
-        if self.sym_wt == 2:
-            if self.wt % 2 == 0:
-                nm = (t ** 10 + t ** 14 + 2 * t ** 16 + t ** 18 - t ** 20 - t ** 26 -
-                      t ** 28 + t ** 32)
-            else:
-                nm = t ** 21 + t ** 23 + t ** 27 + t ** 29 - t ** 33
-            return (nm / dnm)[self.wt]
-        elif self.sym_wt == 4:
-            nm = (1 + t ** 7) * \
-                (t ** 8 + t ** 10 + t ** 12 + t ** 14 + t ** 16)
-            return (nm / dnm)[self.wt]
-
-
-class VectorValuedSMFsSym2(VvsmfSym2_4):
-
-    @cached_method
-    def basis(self):
-        if self.dimension() == 0:
-            return []
-        es4, es6, x10, x12, _ = deg2_ring_gens(self.prec)
-
-        def basis_of_wt(wt, ignore_wts=None):
-            dcts = [dict(zip([4, 6, 10, 12], t))
-                    for t in tuples_even_wt_modular_forms(wt)]
-            if ignore_wts is None:
-                ignore_wts = []
-            dcts = [d for d in dcts if all([d[w] == 0 for w in ignore_wts])]
-            gens_dct = {4: es4, 6: es6, 10: x10, 12: x12}
-            return [mul([gens_dct[k] ** v for k, v in d.iteritems()])
-                    for d in dcts]
-
-        k = self.wt
-
-        if k % 2 == 0:
-            res = []
-            f10 = rankin_cohen_pair_sym(2, es4, es6)
-            f14 = rankin_cohen_pair_sym(2, es4, x10)
-            f16 = rankin_cohen_pair_sym(2, es4, x12)
-            g16 = rankin_cohen_pair_sym(2, es6, x10)
-            f18 = rankin_cohen_pair_sym(2, es6, x12)
-            f22 = rankin_cohen_pair_sym(2, x10, x12)
-            l1 = [f10, f14, f16]
-            l2 = [g16, f18]
-            for f in l1:
-                res.extend([b * f for b in basis_of_wt(k - f.wt)])
-            for f in l2:
-                res.extend([b * f for b in basis_of_wt(k - f.wt,
-                                                       ignore_wts=[4])])
-            res.extend([b * f22 for b in basis_of_wt(k - 22,
-                                                     ignore_wts=[4, 6])])
-            return res
-
-        else:
-            f21 = rankin_cohen_triple_det_sym2(es4, es6, x10)
-            f23 = rankin_cohen_triple_det_sym2(es4, es6, x12)
-            f27 = rankin_cohen_triple_det_sym2(es4, x10, x12)
-            f29 = rankin_cohen_triple_det_sym2(es6, x10, x12)
-
-            res = []
-            l1 = [f21, f23, f27]
-            for f in l1:
-                res.extend([b * f for b in basis_of_wt(k - f.wt)])
-            res.extend([b * f29 for b in basis_of_wt(k - f29.wt,
-                                                     ignore_wts=[4])])
-            return res
-
-
-class VectorValuedSMFsSym4(VvsmfSym2_4):
-
-    @cached_method
-    def basis(self):
-        if self.dimension() == 0:
-            return []
-        es4, es6, x10, x12, _ = deg2_ring_gens(self.prec)
-
-        def basis_of_wt(wt):
-            gens = [es4, es6, x10, x12]
-            res = []
-            for t in tuples_even_wt_modular_forms(wt):
-                res.append(mul([f ** i for i, f in zip(t, gens)]))
-            return res
-
-        k = self.wt
-
-        if k % 2 == 0:
-            f8 = rankin_cohen_pair_sym(4, es4, es4)
-            f10 = rankin_cohen_pair_sym(4, es4, es6)
-            f12 = rankin_cohen_pair_det2_sym(4, es4, es6)
-            f14 = rankin_cohen_pair_sym(4, es4, x10)
-            f16 = rankin_cohen_pair_sym(4, es6, x10)
-            gens = [f8, f10, f12, f14, f16]
-        else:
-            f15 = rankin_cohen_triple_det_sym4(es4, es4, es6)
-            f17 = rankin_cohen_triple_det_sym4(es4, es6, es6)
-            f19 = rankin_cohen_triple_det_sym4(es4, es4, x10)
-            f21 = rankin_cohen_triple_det_sym4(es4, es4, x12)
-            f23 = rankin_cohen_triple_det_sym4(es4, es6, x12)
-            gens = [f15, f17, f19, f21, f23]
-
-        res = []
-        for f in gens:
-            res.extend([b * f for b in basis_of_wt(k - f.wt)])
-        return res
