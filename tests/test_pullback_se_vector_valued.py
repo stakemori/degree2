bbdb32a17e836b35868747ed7ab0a547b1c51451
Sho Takemori <stakemorii@gmail.com>
2015-11-04 18:18:50 +0900

    Add test for x35.

--- tests/test_pullback_se_vector_valued.py
+++ tests/test_pullback_se_vector_valued.py
@@ -4,7 +4,7 @@ from degree2.diff_operator_pullback_vector_valued import (
 from sage.all import (random_matrix, QQ, binomial, identity_matrix, exp,
                       random_vector, symbolic_expression, ZZ, derivative, matrix)
 from degree2.vector_valued_smfs import vector_valued_siegel_modular_forms as vvsmf
-from degree2.scalar_valued_smfs import x12_with_prec
+from degree2.scalar_valued_smfs import x12_with_prec, x35_with_prec
 from unittest import skip
 
 from degree2.standard_l_scalar_valued import tpl_to_half_int_mat
@@ -45,30 +45,37 @@ class TestPullBackVectorValued(unittest.TestCase):
         # cusp form
         f = M.eigenform_with_eigenvalue_t2(QQ(-19200))
         f = f * f[(1, 1, 1), 0] ** (-1)
-        A = matrix([[QQ(1), QQ(1) / QQ(2)], [QQ(1) / QQ(2), QQ(1)]])
+        A = tpl_to_half_int_mat((1, 1, 1))
         D = A
         fc_111 = fc_of_pullback_of_diff_eisen(12, 14, 2, A, D, 1, 1)
         a = fc_111[u1 ** 2]
         self.assertEqual(
             sum(a * b for a, b in zip(monoms, f[(1, 1, 1)].vec)), fc_111 / a)
-        A1 = matrix([[QQ(2), QQ(1) / QQ(2)], [QQ(1) / QQ(2), QQ(3)]])
-        self.assertEqual(sum(a * b for a, b in zip(monoms, f[(2, 1, 3)].vec)),
+        t2 = (2, 1, 3)
+        A1 = tpl_to_half_int_mat(t2)
+        self.assertEqual(sum(a * b for a, b in zip(monoms, f[t2].vec)),
                          fc_of_pullback_of_diff_eisen(12, 14, 2, A1, D, 1, 1) / a)
 
     def test_pullback_diff_eisen_scalar_wt12(self):
         x12 = x12_with_prec(5)
-        A = tpl_to_half_int_mat((1, 1, 1))
-        D = A
-        fc_111 = QQ(fc_of_pullback_of_diff_eisen(10, 12, 0, A, D, 1, 1))
-        A1 = tpl_to_half_int_mat((2, 1, 3))
-        self.assertEqual(fc_of_pullback_of_diff_eisen(10, 12, 0, A1, D, 1, 1),
-                         fc_111 * x12[(2, 1, 3)])
-        A2 = tpl_to_half_int_mat((2, 0, 2))
-        self.assertEqual(fc_of_pullback_of_diff_eisen(10, 12, 0, A2, D, 1, 1),
-                         fc_111 * x12[(2, 0, 2)])
-        A3 = tpl_to_half_int_mat((2, 2, 3))
-        self.assertEqual(fc_of_pullback_of_diff_eisen(10, 12, 0, A3, D, 1, 1),
-                         fc_111 * x12[(2, 2, 3)])
+        self.assert_pullback_scalar_valued(x12, (1, 1, 1),
+                                           [(2, 1, 3), (2, 0, 2), (2, 2, 3)], 10)
+
+    @skip("not ok")
+    def test_pullback_diff_eisen_scalar_wt35(self):
+        x35 = x35_with_prec(10)
+        self.assert_pullback_scalar_valued(x35, (2, 1, 3),
+                                           [(2, -1, 4), (3, -1, 4), (3, -2, 4)], 32)
+
+    def assert_pullback_scalar_valued(self, f, t0, ts, l):
+        D = tpl_to_half_int_mat(t0)
+        f = f * f[t0] ** (-1)
+        a = fc_of_pullback_of_diff_eisen(l, f.wt, 0, D, D, 1, 1)
+        self.assertNotEqual(a, 0)
+        for t in ts:
+            A = tpl_to_half_int_mat(t)
+            self.assertEqual(fc_of_pullback_of_diff_eisen(l, f.wt, 0, A, D, 1, 1),
+                             a * f[t])
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestPullBackVectorValued)
